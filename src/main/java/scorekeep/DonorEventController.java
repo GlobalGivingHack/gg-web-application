3a0368ed9c75fd1e0316dbd6706d9ca5b4fa4ed2
Bilenkin <dimitri.bilenkin@capitalone.com>
2017-11-27 14:55:32 -0800

    fixed donor event controller

--- src/main/java/scorekeep/DonorEventController.java
+++ src/main/java/scorekeep/DonorEventController.java
@@ -2,18 +2,20 @@ package scorekeep;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * Created by rrv162 on 11/27/17.
  */
+@RestController
+@RequestMapping(value="/api/donorEvent")
 public class DonorEventController {
 
     private final DonorEventFactory factory = new DonorEventFactory();
-    private final DonorEventModel model = new DonorEventModel();
 
-    /* GET /user */
+    /* GET /donorEvent */
     @RequestMapping(method=RequestMethod.GET)
     public List<DonorEvent> getDonorEvents() {
         return factory.getDonorEvents();
